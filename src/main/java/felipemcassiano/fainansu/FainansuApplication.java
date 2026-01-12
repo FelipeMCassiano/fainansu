0426765444d7c18fa3d7e7cf81c9e80388bc7b84
FelipeMCassiano <felipemcassiano@gmail.com>
2026-01-12 18:53:11 -0300

    init project

---
+++ src/main/java/felipemcassiano/fainansu/FainansuApplication.java
@@ -0,0 +1,13 @@
+package felipemcassiano.fainansu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FainansuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FainansuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/felipemcassiano/fainansu/FainansuApplicationTests.java
@@ -0,0 +1,13 @@
+package felipemcassiano.fainansu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FainansuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
